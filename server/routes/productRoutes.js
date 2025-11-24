ab6eb00f971dfd79a3570495658ff249b87447ab
ibrahemassem <ibrahem.assem@hotmail.com>
2025-11-24 16:18:11 +0200

    Remove temporary seeding route

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,22 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { getProducts, getProductById } = require('../controllers/productController');
-const Product = require('../models/Product');
-const products = require('../data/products');
 
 router.route('/').get(getProducts);
-
-// Temporary Seeding Route
-router.post('/seed', async (req, res) => {
-  try {
-    await Product.deleteMany({});
-    await Product.insertMany(products);
-    res.json({ message: 'Database seeded successfully!' });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
 router.route('/:id').get(getProductById);
 
 module.exports = router;
