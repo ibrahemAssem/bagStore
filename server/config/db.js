666296721b2e3102c14ddf8da1caa939520d8fa3
ibrahemassem <ibrahem.assem@hotmail.com>
2025-11-24 15:06:52 +0200

    Add DB connection logging

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    console.log(`Attempting to connect to MongoDB...`);
+    console.log(`MONGO_URI is set: ${!!process.env.MONGO_URI}`);
     const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/bagstore');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
